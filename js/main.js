29aab94827bd02484ab1edfdb6af4c678a74d053
Eleonora Hwang <eleonorahwang@gmail.com>
2022-02-14 18:10:14 -0800

    Listen for 'submit' events on the journal entry form and Put the form's input values into a new object.

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ $photoUrl.addEventListener('input', updatePhotoPreview);
 function updatePhotoPreview(event) {
   $photo.setAttribute('src', $photoUrl.value);
 }
+
+var $journalEntry = document.querySelector('#journal-entry');
+
+$journalEntry.addEventListener('submit', createEntry);
+// var nextEntryId = 0;
+function createEntry(event) {
+  event.preventDefault();
+  var entryObj = {};
+  var title = $journalEntry.elements.title.value;
+  var photoUrl = $journalEntry.elements.photoUrl.value;
+  var journalEntry = $journalEntry.elements.journalEntry.value;
+  entryObj.title = title;
+  entryObj.photoUrl = photoUrl;
+  entryObj.journalEntry = journalEntry;
+  // console.log('entryObj: ', entryObj);
+}
